8a4f62d4ab8f348d51245fc09406f308d1122f9f
alvinvgeorge@outlook.com <alvinvgeorge@outlook.com>
2018-05-07 18:27:16 +0530

    * .gitignore: initial
    * SampleTableview.sln:
    * Main.cs:
    * Info.plist:
    * AppDelegate.cs:
    * Main.storyboard:
    * ViewController.cs:
    * Entitlements.plist:
    * MyTableViewCell.cs:
    * mac.png:
    * MyTableViewCell.xib:
    * SampleTableview.csproj:
    * LaunchScreen.storyboard:
    * ViewController.designer.cs:
    * MyTableViewCell.designer.cs:
    * Contents.json:
    * Contents.json:

---
+++ SampleTableview/ViewController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using System.Collections.Generic;
+using ObjCRuntime;
+
+namespace SampleTableview
+{
+    public partial class ViewController : UIViewController, IUITableViewDelegate, IUITableViewDataSource
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            this.sampleTableview.Delegate = this;
+            this.sampleTableview.DataSource = this;
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public nint RowsInSection(UITableView tableView, nint section)
+        {
+            return 10;
+        }
+        [Export("tableView:heightForRowAtIndexPath:")]
+        public nfloat GetHeightForRow(UITableView tableView, NSIndexPath indexPath)
+        {
+            return 200;
+        }
+        [Export("numberOfSectionsInTableView:")]
+        public nint NumberOfSections(UITableView tableView)
+        {
+            return 1;
+        }
+        public UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            UITableViewCell cell = tableView.DequeueReusableCell("cell");
+
+            if (cell == null)
+            {
+                cell = tableView.DequeueReusableCell("MyTableViewCellId");
+            }
+
+
+            return cell;
+        }
+    }
+}
